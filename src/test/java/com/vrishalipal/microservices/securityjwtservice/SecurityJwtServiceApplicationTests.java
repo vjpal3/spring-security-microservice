484a7da3af8a597bd39d91e5c30b3c5ea3a904ae
vjpal3 <vrishalipal@yahoo.com>
2020-05-09 23:17:46 -0400

    Initial commit

---
+++ src/test/java/com/vrishalipal/microservices/securityjwtservice/SecurityJwtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrishalipal.microservices.securityjwtservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
