3bc3c06fa3687865b5768835f51d26c019db7520
vjpal3 <vrishalipal@yahoo.com>
2020-05-15 23:21:04 -0400

    Change jwt exp time

--- src/main/java/com/vrishalipal/microservices/securityjwtservice/security/SecurityConstants.java
+++ src/main/java/com/vrishalipal/microservices/securityjwtservice/security/SecurityConstants.java
@@ -6,5 +6,5 @@ public class SecurityConstants {
 	public static final String SECRET = "MSAJWTsKey"; 
 	public static final String TOKEN_PREFIX = "Bearer "; 
 	public static final String HEADER_STRING = "Authorization"; 
-	public static final long EXPIRATION_TIME = 100_000 * 6 * 60; //1 hour
+	public static final long EXPIRATION_TIME = 120_000; 
 }
